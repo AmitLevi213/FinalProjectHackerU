7be5300690fd09971112e225a66531630185c5d6
AmitLevi213 <amitandta@gmail.com>
2024-07-03 21:20:19 +0300

    fixed playlistdeatilspage

--- Front-end/client/src/music-cards/pages/PlayListDetailsPage.jsx
+++ Front-end/client/src/music-cards/pages/PlayListDetailsPage.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid, IconButton } from "@mui/material";
 import CardComponent from "../components/card/CardComponent";
 import { getCards } from "../service/cardApiService";
 import { useTheme } from "../../providers/DarkThemeProvider";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const PlaylistDetailsPage = () => {
   const { isDark } = useTheme();
   const { genre } = useParams();
   const [cards, setCards] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const cardsPerPage = 4;
   const myColor = isDark ? "#e3f2fd" : "#1a0033";
 
   useEffect(() => {
@@ -21,19 +25,54 @@ const PlaylistDetailsPage = () => {
     fetchData();
   }, [genre]);
 
+  const handleNextPage = () => {
+    if ((currentPage + 1) * cardsPerPage < cards.length) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
   return (
     <Box
       sx={{
-        display: "grid",
-        justifyContent: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: 2,
       }}
     >
-      <Typography sx={{ color: myColor }} variant="h4">
+      <Typography
+        sx={{ color: myColor, marginBottom: 2 }}
+        variant="h4"
+        align="center"
+      >
         {genre} Playlist
       </Typography>
-      {cards.map((card) => (
-        <CardComponent key={card._id} card={card} />
-      ))}
+      <Grid item>
+        {cards
+          .slice(currentPage * cardsPerPage, (currentPage + 1) * cardsPerPage)
+          .map((card) => (
+            <Grid item xs={12} sm={6} md={3} key={card._id}>
+              <CardComponent card={card} />
+            </Grid>
+          ))}
+      </Grid>
+      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
+        <IconButton onClick={handlePreviousPage} disabled={currentPage === 0}>
+          <ArrowBackIcon sx={{ color: myColor }} />
+        </IconButton>
+        <IconButton
+          onClick={handleNextPage}
+          disabled={(currentPage + 1) * cardsPerPage >= cards.length}
+        >
+          <ArrowForwardIcon sx={{ color: myColor }} />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
