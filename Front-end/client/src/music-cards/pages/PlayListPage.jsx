70d9642d198bf58439357be14555d16559498cf7
AmitLevi213 <amitandta@gmail.com>
2024-07-13 15:55:39 +0300

    fixed playlist page

--- Front-end/client/src/music-cards/pages/PlayListPage.jsx
+++ Front-end/client/src/music-cards/pages/PlayListPage.jsx
@@ -7,6 +7,7 @@ import ROUTES from "../../routes/routesModel";
 import { useTheme } from "../../providers/DarkThemeProvider";
 import { useUser } from "../../users/providers/UserProvider";
 import { makeFirstLetterCapital } from "../../forms/utils/upperCaseMethod";
+
 const PlayListPage = () => {
   const { isDark } = useTheme();
   const { user } = useUser();
@@ -19,7 +20,9 @@ const PlayListPage = () => {
     const fetchGenres = async () => {
       try {
         const cards = await getCards();
-        const genreSet = new Set(cards.map((card) => card.genre[0]));
+        const genreSet = new Set(
+          cards.map((card) => card.genre && card.genre[0])
+        );
         setGenres(Array.from(genreSet));
       } catch (error) {
         console.error("Failed to fetch cards", error);
@@ -49,61 +52,63 @@ const PlayListPage = () => {
         gap: 3,
       }}
     >
-      {genres.map((genre) => (
-        <Box
-          key={genre}
-          sx={{
-            position: "relative",
-            width: 200,
-            height: 200,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "1px solid #ccc",
-            borderRadius: 2,
-            boxShadow: 3,
-            transition: "transform 0.3s",
-            "&:hover": {
-              transform: "scale(1.05)",
-            },
-            "&:hover .add-button": {
-              opacity: 1,
-            },
-          }}
-          onClick={() => handleGenreClick(genre)}
-        >
-          <Typography variant="h6" component="div" sx={{ color: textColor }}>
-            {makeFirstLetterCapital(genre)}
-          </Typography>
-          {isBusinessOrAdmin && (
-            <Button
-              variant="contained"
-              color="primary"
-              startIcon={<AddIcon />}
-              className="add-button"
-              sx={{
-                position: "absolute",
-                bottom: 16,
-                right: 16,
-                opacity: {
-                  xs: 1,
-                  sm: 1,
-                  md: 0,
-                },
-                transition: "opacity 0.5s",
-                backgroundColor: buttonColor,
-                color: isDark ? "#1a0033" : "#e3f2fd",
-                "&:hover": {
-                  backgroundColor: isDark ? "#e3f2fd" : "#1a0033",
-                },
-              }}
-              onClick={handleAddClick}
-            >
-              Add
-            </Button>
-          )}
-        </Box>
-      ))}
+      {genres
+        .filter((genre) => genre)
+        .map((genre) => (
+          <Box
+            key={genre}
+            sx={{
+              position: "relative",
+              width: 200,
+              height: 200,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "1px solid #ccc",
+              borderRadius: 2,
+              boxShadow: 3,
+              transition: "transform 0.3s",
+              "&:hover": {
+                transform: "scale(1.05)",
+              },
+              "&:hover .add-button": {
+                opacity: 1,
+              },
+            }}
+            onClick={() => handleGenreClick(genre)}
+          >
+            <Typography variant="h6" component="div" sx={{ color: textColor }}>
+              {makeFirstLetterCapital(genre)}
+            </Typography>
+            {isBusinessOrAdmin && (
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<AddIcon />}
+                className="add-button"
+                sx={{
+                  position: "absolute",
+                  bottom: 16,
+                  right: 16,
+                  opacity: {
+                    xs: 1,
+                    sm: 1,
+                    md: 0,
+                  },
+                  transition: "opacity 0.5s",
+                  backgroundColor: buttonColor,
+                  color: isDark ? "#1a0033" : "#e3f2fd",
+                  "&:hover": {
+                    backgroundColor: isDark ? "#e3f2fd" : "#1a0033",
+                  },
+                }}
+                onClick={handleAddClick}
+              >
+                Add
+              </Button>
+            )}
+          </Box>
+        ))}
     </Box>
   );
 };
