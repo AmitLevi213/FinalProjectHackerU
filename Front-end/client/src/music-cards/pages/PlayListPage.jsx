275d8d47498b01708e55024dcbb3232cb52cdcc4
AmitLevi213 <amitandta@gmail.com>
2024-07-28 01:10:59 +0300

    added PageHeader in Playlistpage

--- Front-end/client/src/music-cards/pages/PlayListPage.jsx
+++ Front-end/client/src/music-cards/pages/PlayListPage.jsx
@@ -7,6 +7,7 @@ import ROUTES from "../../routes/routesModel";
 import { useTheme } from "../../providers/DarkThemeProvider";
 import { useUser } from "../../users/providers/UserProvider";
 import { makeFirstLetterCapital } from "../../forms/utils/upperCaseMethod";
+import PageHeader from "../../components/PageHeader";
 
 const PlayListPage = () => {
   const { isDark } = useTheme();
@@ -44,72 +45,82 @@ const PlayListPage = () => {
   const isBusinessOrAdmin = user && (user.isBusiness || user.isAdmin);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        flexWrap: "wrap",
-        gap: 3,
-      }}
-    >
-      {genres
-        .filter((genre) => genre)
-        .map((genre) => (
-          <Box
-            key={genre}
-            sx={{
-              position: "relative",
-              width: 200,
-              height: 200,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              border: "1px solid #ccc",
-              borderRadius: 2,
-              boxShadow: 3,
-              transition: "transform 0.3s",
-              "&:hover": {
-                transform: "scale(1.05)",
-              },
-              "&:hover .add-button": {
-                opacity: 1,
-              },
-            }}
-            onClick={() => handleGenreClick(genre)}
-          >
-            <Typography variant="h6" component="div" sx={{ color: textColor }}>
-              {makeFirstLetterCapital(genre)}
-            </Typography>
-            {isBusinessOrAdmin && (
-              <Button
-                variant="contained"
-                color="primary"
-                startIcon={<AddIcon />}
-                className="add-button"
-                sx={{
-                  position: "absolute",
-                  bottom: 16,
-                  right: 16,
-                  opacity: {
-                    xs: 1,
-                    sm: 1,
-                    md: 0,
-                  },
-                  transition: "opacity 0.5s",
-                  backgroundColor: buttonColor,
-                  color: isDark ? "#1a0033" : "#e3f2fd",
-                  "&:hover": {
-                    backgroundColor: isDark ? "#e3f2fd" : "#1a0033",
-                  },
-                }}
-                onClick={handleAddClick}
+    <>
+      <PageHeader
+        title="Playlist page"
+        subtitle="Here you can find a variety of different geners"
+      />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: 3,
+        }}
+      >
+        {genres
+          .filter((genre) => genre)
+          .map((genre) => (
+            <Box
+              key={genre}
+              sx={{
+                position: "relative",
+                width: 200,
+                height: 200,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                border: "1px solid #ccc",
+                borderRadius: 2,
+                boxShadow: 3,
+                transition: "transform 0.3s",
+                "&:hover": {
+                  transform: "scale(1.05)",
+                },
+                "&:hover .add-button": {
+                  opacity: 1,
+                },
+              }}
+              onClick={() => handleGenreClick(genre)}
+            >
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ color: textColor }}
               >
-                Add
-              </Button>
-            )}
-          </Box>
-        ))}
-    </Box>
+                {makeFirstLetterCapital(genre)}
+              </Typography>
+              {isBusinessOrAdmin && (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<AddIcon />}
+                  className="add-button"
+                  sx={{
+                    position: "absolute",
+                    bottom: 16,
+                    right: 16,
+                    opacity: {
+                      xs: 1,
+                      sm: 1,
+                      md: 0,
+                    },
+                    transition: "opacity 0.5s",
+                    backgroundColor: buttonColor,
+                    color: isDark ? "#1a0033" : "#e3f2fd",
+                    "&:hover": {
+                      backgroundColor: isDark ? "#e3f2fd" : "#1a0033",
+                    },
+                  }}
+                  onClick={handleAddClick}
+                >
+                  Add
+                </Button>
+              )}
+            </Box>
+          ))}
+      </Box>
+    </>
   );
 };
 
