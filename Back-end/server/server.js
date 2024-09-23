9d18ed1d9157138b663b1976ac840cb3d1b0f9ec
AmitLevi213 <amitandta@gmail.com>
2024-09-23 23:58:48 +0300

    trying to upload to air

--- Back-end/server/server.js
+++ Back-end/server/server.js
@@ -12,7 +12,7 @@ const { generateInitialData } = require("./initialData/initialDataService");
 app.use(cors);
 app.use(logger);
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static("./public/assets"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
