04a94ccd1087f66e68e95a3f612959ec9a53b029
AmitLevi213 <amitandta@gmail.com>
2024-09-23 23:39:39 +0300

    trying to upload to air

--- Back-end/server/server.js
+++ Back-end/server/server.js
@@ -13,6 +13,10 @@ app.use(cors);
 app.use(logger);
 app.use(express.json());
 app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.use(router);
 
 app.use((err, req, res, next) => {
