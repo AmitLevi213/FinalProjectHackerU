13450cd8d523fb2f93b80c6b9a9bb5d6e352915b
AmitLevi213 <amitandta@gmail.com>
2024-11-12 12:37:52 +0200

    attempt deployment to Air(4)

--- Back-end/server/middlewares/cors.js
+++ Back-end/server/middlewares/cors.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
+
+const allowedOrigins = [
+  "http://127.0.0.1:5500",
+  "http://localhost:3000",
+  "https://finalprojecthackeru-cilent.onrender.com",
+  "https://finalprojecthackeru-cilent.onrender.com/users",
+  "https://finalprojecthackeru-cilent.onrender.com/cards",
+];
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:3000"],
+    origin: allowedOrigins,
     optionsSuccessStatus: 200,
   })
 );
