b02b8ddee42d7760a833391d292fac5e39e7fe46
AmitLevi213 <amitandta@gmail.com>
2024-12-23 18:50:34 +0200

    Production

--- Back-end/server/middlewares/cors.js
+++ Back-end/server/middlewares/cors.js
@@ -7,12 +7,16 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3002",
+  "https://mp3-storage-58830.web.app",
+  "https://mp3-storage-58830.firebaseapp.com",
 ];
 
 app.use(
   cors({
     origin: allowedOrigins,
     optionsSuccessStatus: 200,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
